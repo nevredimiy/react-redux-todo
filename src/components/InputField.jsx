e0ff911bf0d1cc03e1e1ebbd21c4744da79d2b06
unknown <artemlitvinov81@gmail.com>
2023-08-18 14:16:41 +0300

    React + Redux  Todo Task from Mihail Nepomnyaschiy

---
+++ src/components/InputField.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const InputField = ({text, handleSubmit, handleInput}) => {
+  return (
+    <label>
+        <input type="text" value={text} onChange={(e) => handleInput(e.target.value)} />
+        <button onClick={handleSubmit}>Add Todo</button>
+      </label>
+  )
+}
+
+export default InputField
